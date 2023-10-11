3be1ed326e6b5a5f5a21d2817bf70365d2d3bcd5
MrEgor123 <144677952+MrEgor123@users.noreply.github.com>
2023-10-11 19:00:28 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,18 @@
+﻿var
+x, rez: real;
+begin
+
+writeln('Введите значение x: ');
+readln(x);
+
+if x < -6 then
+rez := x * x / tan(x)
+else if (x >= -6) and (x < 1) then
+rez := ln(x) / (0.1 * x) - ln(x) / (x * x)
+else if (x >= 1) and (x < 5) then
+rez := exp(x) / tan(x) - 86 / ln(x)
+else
+rez := 7;
+
+writeln('f (', x:0:2, ') = ', rez:0:2);
+end.
